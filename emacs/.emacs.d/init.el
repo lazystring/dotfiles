5772e56935bcda46b7cc586a4d41271bd64a1a9a
lazystring <johnbojorquez36@gmail.com>
2022-07-10 18:17:10 -0400

    Some updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,18 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(push "~/.dotfiles/emacs/.emacs.d/lisp" load-path)
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 ;; Thanks, but no thanks
 (setq inhibit-startup-message t)
 
@@ -58,19 +73,6 @@
    (doom-modeline-buffer-file-name-style 'truncate-except-project)
    (doom-modeline-major-mode-icon nil)
    (doom-modeline-icon (display-graphic-p))))
-
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
 
 (setq-default tab-width 2)
 (setq-default evil-shift-width tab-width)
@@ -449,46 +451,6 @@
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'ls/org-babel-tangle-config)))
 
-(use-package mu4e
-  :ensure nil
-  :load-path "/usr/share/emacs/site-lisp/mu4e/"
-  :defer 20 ; Wait until 20 seconds after startup
-  :config
-
-  ;; This is set to 't' to avoid mail syncing issues when using mbsync
-  (setq mu4e-change-filenames-when-moving t)
-
-  ;; Refresh mail using isync every 10 minutes
-  (setq mu4e-update-interval (* 10 60))
-  (setq mu4e-get-mail-command "mbsync -a")
-  (setq mu4e-maildir "~/Mail")
-
-  (setq mu4e-drafts-folder "/[Gmail]/Drafts")
-  (setq mu4e-sent-folder   "/[Gmail]/Sent Mail")
-  (setq mu4e-refile-folder "/[Gmail]/All Mail")
-  (setq mu4e-trash-folder  "/[Gmail]/Trash")
-
-  (setq smtpmail-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-service 465
-        smtpmail-stream-type 'ssl)
-
-  (setq user-mail-address "johnbojorquez36@gmail.com")
-  (setq user-full-name "John Bojorquez")
-  (setq message-send-mail-function 'smtpmail-send-it)
-
-  (setq mu4e-maildir-shortcuts
-        '((:maildir "/Inbox"    :key ?i)
-          (:maildir "/[Gmail]/Sent Mail" :key ?s)
-          (:maildir "/[Gmail]/Trash"     :key ?t)
-          (:maildir "/[Gmail]/Drafts"    :key ?d)
-          (:maildir "/[Gmail]/All Mail"  :key ?a)))
-
-  (setq mu4e-bookmarks
-        '((:name "Unread messages" :query "flag:unread AND NOT flag:trashed" :key ?i)
-          (:name "Today's messages" :query "date:today..now" :key ?t)
-          (:name "Bank deposits" :query "from:email@alerts.ally.com subject:\"A deposit was made\"" :key ?d)
-          (:name "Last 7 days" :query "date:7d..now" :hide-unread t :key ?w)
-          (:name "Messages with images" :query "mime:image/*" :key ?p)))
-
-  ;; Run in background to sync periodically.
-  (mu4e t))
+(setq ls/mail-enabled (member system-name '("lazylaptop")))
+(when ls/mail-enabled
+  (require 'ls-mail))
